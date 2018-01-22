fc9f194c80092560a579d1560cb1def9d24d917d
Julien PETIT <contact@julienpetit.fr>
2018-01-22 18:14:49 +0100

    Update webpack config and add configure manifest.json in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,21 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
+// directory where all compiled assets will be stored
     .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
+    // what's the public path to this directory (relative to your project's document root dir)
     .setPublicPath('/build')
+    // empty the outputPath dir before each build
     .cleanupOutputBeforeBuild()
+    // will output as web/build/app.js
+    .addEntry('app', ['babel-polyfill', 'whatwg-fetch', './assets/js/app.js'])
+    // will output as web/build/app.css
+    // .addStyleEntry('css/main', './assets/sass/layout.scss')
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+    // create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
 
-    // uncomment to define the assets of the project
-    .addEntry('app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
-
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
-;
-
-module.exports = Encore.getWebpackConfig();
+// export the final configuration
+module.exports = Encore.getWebpackConfig()
